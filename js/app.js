0dbec70d6369d921f79a4c3a84ecf6c73d68b806
Caoimhe Clarke <caoimhepower@gmail.com>
2018-11-20 17:39:03 +0000

    scrol reveal added to project images

--- js/app.js
+++ js/app.js
@@ -5,8 +5,6 @@ $(() => {
   const $navul = $('nav ul');
   const $menulink = $('.nav-menu a');
 
-  // if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-
   $togglenav.on('click', () => {
     $togglenav.toggleClass('active');
     $navul.toggleClass('active');
@@ -16,6 +14,5 @@ $(() => {
     $togglenav.toggleClass('active');
     $navul.toggleClass('active');
   });
-  // }
 
 });
