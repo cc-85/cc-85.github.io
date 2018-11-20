89ba92fd72fea5ee0d15c2739acd61e9c9da2aba
Caoimhe Clarke <caoimhepower@gmail.com>
2018-11-20 14:28:17 +0000

    mobile nav fixed position

--- js/app.js
+++ js/app.js
@@ -1,22 +1,5 @@
 console.log('JS loaded');
 
-// const $navBurger = document.querySelector('.toggle-nav');
-//
-// if ($navBurger) {
-//
-// }
-
-
-// jQuery(document).ready(function() {
-// 	jQuery('.toggle-nav').click(function(e) {
-// 		jQuery(this).toggleClass('active');
-// 		jQuery('nav ul').toggleClass('active');
-//
-// 		e.preventDefault();
-// 	});
-// });
-
-
 $(() => {
   const $togglenav = $('.toggle-nav');
   const $navul = $('nav ul');
@@ -24,7 +7,7 @@ $(() => {
 
   if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
 
-    $togglenav.on('touchstart', () => {
+    $togglenav.on('click', () => {
       $togglenav.toggleClass('active');
       $navul.toggleClass('active');
     });
